dd3bfe59f7e2572a908ebe15f948a603aa4e6437
Rahul Vijan <69571769+rv2442@users.noreply.github.com>
2022-02-02 18:19:51 +0530

    Add files via upload

---
+++ texttospeechbot-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\texttospeechbot-bot-9e49-Web-Deploy.pubxml -p:Password=jCt83JuoH0Djp7hpsbi6puxxlQLzvaBXiu5vBhHGJdoXK29phFaYdBa6w4e0
+
